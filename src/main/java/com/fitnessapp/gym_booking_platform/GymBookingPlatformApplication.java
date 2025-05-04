91cae360f163739ba637ad78daa94e271cd9887a
Apoorav Singh <apooravsingh38@gmail.com>
2025-05-04 20:22:41 +0530

    "Setup"

---
+++ src/main/java/com/fitnessapp/gym_booking_platform/GymBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.gym_booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessapp/gym_booking_platform/GymBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.gym_booking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
